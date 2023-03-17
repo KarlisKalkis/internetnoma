1efe59fff45899115e129cf263a191eed689628e
KarlisKalkis <kkalkis21@gmail.com>
2023-03-17 20:01:25 +0200

    salabots izkstas tractor lapa

--- tractorsAgriculture.php
+++ tractorsAgriculture.php
@@ -21,28 +21,32 @@ if ($stmtselect->execute()) {
     echo 'there were errors saving data';
 } ?>
 
-<div class="container mt-3 p-5">
-    <div class="row d-flex">
-        <div class="col-md-12 pb-3">
-            <?php foreach ($productstractors as $product) : ?>
+<div class="container mt-3 pt-3  ">
+    <div class="row">
+        <?php foreach ($productstractors as $product) : ?>
+        <div class="col-md-6 pb-4 d-flex">
+            
 
-                <div class="card-sl flex-row">
+                <div class="card-sl flex-row px-lg-4 pb-4 col-sm-10">
 
 
-                    <div class="card-image img-thumbnail bg-black ">
+                    <div class="card-image img-thumbnail bg-black w-100">
                         <img src="<?php echo $product['photolocation'] ?>" />
                     </div>
 
 
                     <div class="card-heading"><?php echo $product['name'] ?></div>
                     <div class="card-text"><?php echo $product['shortinfo'] ?></div>
-                    <div class="card-text"><?php echo $product['price'] ?></div>
-                    <a href="products/9R390.html" class="card-button bg-primary text-decoration-none">Rent</a>
+                    <div class="card-text"><?php echo $product['price'] ?> EIRO</div>
+                    <a href="<?php echo $product['rentpage']?>" class="card-button bg-primary text-decoration-none">Rent</a>
 
                 </div>
-            <?php endforeach ?>
+            
 
         </div>
+        <?php endforeach ?>
+    </div>
+</div>
 
 
 
